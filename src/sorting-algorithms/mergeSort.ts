ecebd194c499911c08641774e997213db677860b
Cole Michaels <cmichaelsd@gmail.com>
2021-01-08 16:07:14 +0900

    implemented merge sort.

---
+++ src/sorting-algorithms/mergeSort.ts
@@ -0,0 +1,55 @@
+function merge(arr: number[], leftIndex: number, middleIndex: number, rightIndex: number): void {
+    const leftRange: number = middleIndex - leftIndex + 1;
+    const rightRange: number = rightIndex - middleIndex;
+    const leftArray: number[] = [];
+    const rightArray: number[] = [];
+
+    for (let i: number = 0; i < leftRange; ++i) {
+        leftArray[i] = arr[leftIndex + i];
+    }
+
+    for (let i: number = 0; i < rightRange; ++i) {
+        rightArray[i] = arr[middleIndex + 1 + i];
+    }
+
+    let leftArrayIndex: number = 0;
+    let rightArrayIndex: number = 0;
+    let arrayIndex: number = leftIndex;
+
+    while (leftArrayIndex < leftRange && rightArrayIndex < rightRange) {
+        if (leftArray[leftArrayIndex] <= rightArray[rightArrayIndex]) {
+            arr[arrayIndex] = leftArray[leftArrayIndex];
+            ++leftArrayIndex;
+        } else {
+            arr[arrayIndex] = rightArray[rightArrayIndex];
+            ++rightArrayIndex;
+        }
+
+        ++arrayIndex;
+    }
+
+    while (leftArrayIndex < leftRange) {
+        arr[arrayIndex] = leftArray[leftArrayIndex];
+        ++leftArrayIndex;
+        ++arrayIndex;
+    }
+
+    while (rightArrayIndex < rightRange) {
+        arr[arrayIndex] = leftArray[rightArrayIndex];
+        ++rightArrayIndex;
+        ++arrayIndex;
+    }
+}
+
+function mergeSort(arr: number[], leftIndex: number, rightIndex: number): void {
+    if (leftIndex < rightIndex) {
+        const middleIndex: number = Math.floor(leftIndex + (rightIndex - leftIndex) / 2);
+
+        // Sort left
+        mergeSort(arr, leftIndex, middleIndex);
+        // Sort right
+        mergeSort(arr, middleIndex + 1, rightIndex);
+        // Merge
+        merge(arr, leftIndex, middleIndex, rightIndex);
+    }
+}
